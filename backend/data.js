6b94eb079d14fc02feebb3ce9e0647beffb2cdbc
Anastasia Ryzhova <anastasiawarm@gmail.com>
2021-05-15 16:14:37 +0300

    initial commit

---
+++ backend/data.js
@@ -0,0 +1,26 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Lada manual 5 speed shift',
+      category: 'Parts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Lada',
+      description: 'high quaity product'
+    },
+    {
+      _id: '2',
+      name: 'Lada manual 5 speed shift #2',
+      category: 'Parts',
+      image: '/images/p1.jpg',
+      price: 130,
+      countInStock: 0,
+      brand: 'Lada',
+      description: 'Very high quaity product'
+    }
+  ]
+};
+
+module.exports = data;
